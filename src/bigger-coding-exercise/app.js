3c49f537168a33a1ba83608c20b93dc1c4bde5d3
Punit Shah <punit@punitshah.dev>
2023-11-09 18:23:46 +0000

    Add features from interview
    - Main image is updated when one of the image links is clicked
    - Breed search added

--- src/bigger-coding-exercise/app.js
+++ src/bigger-coding-exercise/app.js
@@ -26,13 +26,28 @@ async function getMultipleRandomDogImages(amount) {
   }
 }
 
+async function getImagesByBreed(breed) {
+  try {
+    const response = await fetch(
+      `https://dog.ceo/api/breed/${breed}/images/random/10`
+    );
+    if (!response.ok) {
+      throw { status: response.status };
+    }
+    const data = await response.json();
+    return data.message;
+  } catch (error) {
+    console.error('Error requesting dog image API', error);
+  }
+}
+
 // extract the breed from a dog image url
 function getBreedFromUrl(url) {
   const [_, breedUrlMatch] = url.match(/\/breeds\/(.*)\//);
   return breedUrlMatch.split('-').reverse().join(' ');
 }
 
-async function updateMainImage() {
+async function initialiseMainImage() {
   const container = document.getElementById('main-image-container');
 
   container.textContent = 'Loading...';
@@ -48,12 +63,14 @@ async function updateMainImage() {
   container.append(image);
 }
 
-async function updateImageLinks() {
+// updates the image links with a new array of image urls
+// if the urls array is not provided, 10 random dog images are fetched instead
+async function updateImageLinks(urls) {
   const container = document.getElementById('image-links-container');
 
   container.textContent = 'Loading...';
 
-  const imageUrls = await getMultipleRandomDogImages(10);
+  const imageUrls = urls || (await getMultipleRandomDogImages(10));
 
   const list = document.createElement('ul');
   imageUrls.forEach((url) => {
@@ -63,6 +80,11 @@ async function updateImageLinks() {
     const link = document.createElement('a');
     link.href = url;
     link.target = '_blank';
+
+    link.onclick = (event) => {
+      event.preventDefault();
+      updateMainImage(url, breed);
+    };
 
     const image = document.createElement('img');
     image.src = url;
@@ -78,4 +100,29 @@ async function updateImageLinks() {
 
   container.textContent = '';
   container.append(list);
+}
+
+function updateMainImage(url, alt) {
+  const image = document.querySelector('#main-image-container img');
+
+  image.src = url;
+  image.alt = alt;
+}
+
+function initialiseSearch() {
+  const form = document.getElementById('breed-search');
+
+  form.onsubmit = async (event) => {
+    event.preventDefault();
+    const input = form.querySelector('input');
+    const imageUrls = await getImagesByBreed(input.value);
+
+    updateImageLinks(imageUrls);
+  };
+}
+
+function initialise() {
+  initialiseMainImage();
+  updateImageLinks();
+  initialiseSearch();
 }
